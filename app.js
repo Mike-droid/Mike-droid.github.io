86cf989f8e3a0f66f3c3d867093c4092c515c47e
Miguel Angel Reyes Moreno <miguelreyesmoreno@hotmail.com>
2021-03-27 22:35:18 -0500

    Estilos implementados

--- app.js
+++ app.js
@@ -2,37 +2,53 @@ const baseUrl = `https://pokeapi.co/api/v2/pokemon/`;
 const pokemon = document.getElementById('pokemonName');
 const buttonPokemon = document.getElementById('searchPokemon');
 const buttonDelete = document.getElementById('deleteSearch');
+const buttonClear = document.getElementById('clearPokemon');
 const appNode = document.getElementById('app');
 
 buttonPokemon.addEventListener('click' , () => {
   window.fetch(`${baseUrl}${pokemon.value}`)
-  .then(response => response.json())
-  .then(responseJSON => {
-    const allItems = [];
+    .then(response => response.json())
+    .then(responseJSON => {
+      const allItems = [];
 
-    const result = []; //*Guardaremos la respuesta en el array
+      const result = []; //*Guardaremos la respuesta en el array
 
-    for (let index in responseJSON) { //*Convertimos el objeto JSON a array
-      result.push([index , responseJSON[index]]);
-    }
+      for (let index in responseJSON) { //*Convertimos el objeto JSON a array
+        result.push([index , responseJSON[index]]);
+      }
 
-    console.log(result);
+      console.log(result);
 
-    //*Crear imagen
-    const pokemonImage = document.createElement('img');
-    pokemonImage.src = result[14][1].front_default;
+      //*Crear imagen
+      const pokemonImage = document.createElement('img');
+      pokemonImage.src = result[14][1].front_default; //*Image of pokemon
 
-    //*Tipo de pokemon
-    const pokemonType = document.createElement('h2');
-    pokemonType.innerText = result[16][1][0].type.name;
+      //*Nombre de pokemon
+      const pokemonName = document.createElement('h2');
+      pokemonName.innerText = `Name: ${result[10][1]}`; //* Name of pokemon
 
-    //*Crear contenedor
-    const container = document.createElement('div');
-    container.append(pokemonImage , pokemonType);
-    container.classList.add('container');
+      //*Tipo de pokemon
+      const pokemonType = document.createElement('h2');
+      pokemonType.innerText = `Type: ${result[16][1][0].type.name}`; //*Type of pokemon
 
-    allItems.push(container);
+      //*Crear contenedor
+      const container = document.createElement('div');
+      container.append(pokemonImage , pokemonName ,pokemonType);
+      container.classList.add('container');
 
-    appNode.append(...allItems);
+      allItems.push(container);
+
+      appNode.append(...allItems);
+
+      hasChild = true;
+    });
+});
+
+buttonClear.addEventListener('click' , ()=>{
+  allChildren = appNode.childNodes;
+  allChildren = Array.from(allChildren);
+
+  allChildren.forEach(pokemon => {
+    pokemon.remove(pokemon);
   });
 });
